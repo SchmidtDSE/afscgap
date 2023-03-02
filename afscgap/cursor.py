657cdaeb56efecf470a14c50a304493784365ab2
Sam Pottinger <sam.pottinger@berkeley.edu>
2023-03-02 14:37:35 -0800

    Update cursor.py docstring for #16.

--- afscgap/cursor.py
+++ afscgap/cursor.py
@@ -1,6 +1,11 @@
 """
+Interfaces for cursor objects which iterate over real or inferred records.
+
 (c) 2023 Regents of University of California / The Eric and Wendy Schmidt Center
 for Data Science and the Environment at UC Berkeley.
+
+This file is part of afscgap released under the BSD 3-Clause License. See
+LICENSE.txt.
 """
 
 import queue
