c6dfb0732e16051a823ea3bfb6c02d21319b3345
Sam Pottinger <sam.pottinger@berkeley.edu>
2023-03-06 15:47:41 -0800

    Add tests for viz model.

---
+++ afscgapviz/test/test_model.py
@@ -0,0 +1,100 @@
+import unittest
+
+import afscgapviz.model
+
+
+class ModelTests(unittest.TestCase):
+
+    def setUp(self):
+        self._test_record_1 = afscgapviz.model.SimplifiedRecord(
+            2023,
+            'GOA',
+            'scientific',
+            'common',
+            'abc',
+            1.2,
+            3.4,
+            5,
+            6,
+            7,
+            8
+        )
+
+        self._test_record_2 = afscgapviz.model.SimplifiedRecord(
+            2023,
+            'GOA',
+            'scientific',
+            'common',
+            'abc',
+            1.3,
+            3.5,
+            6,
+            7,
+            8,
+            9
+        )
+
+        self._test_record_3 = afscgapviz.model.SimplifiedRecord(
+            2022,
+            'GOA',
+            'scientific',
+            'common',
+            'abc',
+            1.3,
+            3.5,
+            6,
+            7,
+            8,
+            9
+        )
+
+        self._combine_result = self._test_record_1.combine(self._test_record_2)
+
+    def test_get_key(self):
+        self.assertEquals(
+            self._test_record_1.get_key(),
+            self._test_record_2.get_key()
+        )
+
+        self.assertNotEquals(
+            self._test_record_1.get_key(),
+            self._test_record_3.get_key()
+        )
+
+    def test_combine_record_meta(self):
+        self.assertEqual(
+            self._combine_result.get_key(),
+            self._test_record_1.get_key()
+        )
+
+        self.assertEquals(self._combine_result.get_year(), 2023)
+        self.assertEquals(self._combine_result.get_survey(), 'GOA')
+        self.assertEquals(self._combine_result.get_species(), 'scientific')
+        self.assertEquals(self._combine_result.get_common_name(), 'common')
+        self.assertEquals(self._combine_result.get_geohash(), 'abc')
+        
+    def test_combine_record_calculations(self):
+        self.assertAlmostEquals(
+            self._combine_result.get_surface_temperature(),
+            (1.2 * 8 + 1.3 * 9) / (8 + 9)
+        )
+        self.assertAlmostEquals(
+            self._combine_result.get_bottom_temperature(),
+            (3.4 * 8 + 3.5 * 9) / (8 + 9)
+        )
+        self.assertAlmostEquals(
+            self._combine_result.get_weight(),
+            5 + 6
+        )
+        self.assertAlmostEquals(
+            self._combine_result.get_count(),
+            6 + 7
+        )
+        self.assertAlmostEquals(
+            self._combine_result.get_area_swept(),
+            7 + 8
+        )
+        self.assertAlmostEquals(
+            self._combine_result.get_num_records_aggregated(),
+            8 + 9
+        )
