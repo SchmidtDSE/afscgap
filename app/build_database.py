503070e807153ec2b9716f6453f13dec763b48aa
Sam Pottinger <sam.pottinger@berkeley.edu>
2023-03-06 11:29:28 -0800

    Scaffolding for build database for app.

---
+++ app/build_database.py
@@ -0,0 +1,239 @@
+"""Script to build a sqlite database with geohashed presence information.
+
+Script / command line utility which builds a sqlite database with geohashed
+presence information. Note that this is not required for use of the afscgap
+library and, instead, is part of the deployment pipeline for a web application
+used for geohash based visualization. For continued development, this is
+considered to be client code of the afscgap library.
+
+(c) 2023 Regents of University of California / The Eric and Wendy Schmidt Center
+for Data Science and the Environment at UC Berkeley.
+
+This file is part of afscgap released under the BSD 3-Clause License. See
+LICENSE.txt.
+"""
+import contextlib
+import re
+import sys
+import typing
+
+import afscgap
+import afscgap.model
+import geolib.geohash
+import requests
+import toolz.itertoolz
+
+import model
+
+INVALID_GEOHASH_STR = 'Expected geohash size to be an integer between 1 - 12.'
+USAGE_BASE_NUM_ARGS = 1
+USAGE_BASE_STR = 'python build_database.py'
+USAGE_COMMANDS_STR = '[create_db | download]'
+USAGE_CREATE_DB_COMMAND = 'create_db'
+USAGE_CREATE_DB_NUM_ARGS = 1
+USAGE_CREATE_DB_STR = 'create_db [sqlite file]'
+USAGE_DOWNLOAD_COMMAND = 'download'
+USAGE_DOWNLOAD_NUM_ARGS = 3
+USAGE_DOWNLOAD_STR = 'download [year range] [sqlite file] [geohash size]'
+YEAR_PATTERN = re.compile('(?P<start>\\d{4})-(?P<end>\\d{4})')
+YEAR_RANGE_STR = 'Year range should be like 2000-2023.'
+
+HAULS = typing.List[afscgap.model.Haul]
+OPT_SIMPLIFIED_RECORD = typing.Optional[model.SimplifiedRecord]
+SIMPLIFIED_RECORDS = typing.Iterable[model.SimplifiedRecord]
+
+
+def try_parse_int(target: str) -> typing.Optional[int]:
+    try:
+        return int(target)
+    except ValueError:
+        return None
+
+
+def try_parse_range(target: str) -> typing.Optional[typing.Tuple[int]]:
+    match = YEAR_PATTERN.match(target)
+    if match is None:
+        return None
+    else:
+        return (int(match.group('start')), int(match.group('end')))
+
+
+def simplify_record(target: afscgap.model.Record,
+    geohash_size: int) -> OPT_SIMPLIFIED_RECORD:
+    latitude = target.get_latitude_dd()
+    longitude = target.get_longitude_dd()
+    geohash = geolib.geohash.encode(latitude, longitude, geohash_size)
+    
+    surface_temperature_c_maybe = target.get_surface_temperature_c_maybe()
+    if surface_temperature_c_maybe is None:
+        return None
+    
+    bottom_temperature_c_maybe = target.get_bottom_temperature_c_maybe()
+    if bottom_temperature_c_maybe is None:
+        return None
+    
+    weight_kg_maybe = target.get_weight_kg_maybe()
+    if weight_kg_maybe is None:
+        return None
+    
+    count_maybe = target.get_count_maybe()
+    if count_maybe is None:
+        return None
+
+    return SimplifiedRecord(
+        target.get_year(),
+        target.get_scientific_name(),
+        target.get_common_name(),
+        geohash,
+        surface_temperature_c_maybe,
+        bottom_temperature_c_maybe,
+        weight_kg_maybe,
+        count_maybe,
+        target.area_swept_ha(),
+        1
+    )
+
+
+def combine_record(a: dict, b: dict) -> dict:
+    assert a['key'] == b['key']
+    return {
+        'key': a['key'],
+        'record': a['record'].combine(b['record'])
+    }
+
+
+def get_year(year: int, geohash_size: int, hauls: HAULS) -> SIMPLIFIED_RECORDS:
+    results = afscgap.query(
+        year=year,
+        presence_only=False,
+        hauls_prefetch=hauls
+    )
+
+    simplified_records_maybe = map(
+        lambda x: simplify_record(x, geohash_size),
+        results
+    )
+    simplified_records = filter(
+        lambda x: x is not None,
+        simplified_records_maybe
+    )
+    keyed_records = map(
+        lambda x: {'key': x.get_key(), 'record': x},
+        simplified_records
+    )
+    records_by_geohash = toolz.reduceby(
+        'key',
+        combine_record,
+        keyed_records
+    )
+
+    return records_by_geohash.values()
+
+
+def get_sql(script_name: str) -> str:
+    parent_dir = pathlib.Path(__file__).parent.absolute()
+    data_dir = os.path.join(parent_dir, 'data')
+    full_path = os.path.join(data_dir, filename)
+    
+    with open(full_path) as f:
+        contents = f.read()
+
+    return contents
+
+
+def create_table(cursor: sqlite3.Cursor):
+    sql = get_sql('create_table')
+    cursor.execute(sql)
+
+
+def record_to_tuple(target: model.SimplifiedRecord) -> typing.Tuple:
+    return (
+        target.get_year(),
+        target.get_survey(),
+        target.get_species(),
+        target.get_common_name(),
+        target.get_geohash(),
+        target.get_surface_temperature(),
+        target.get_bottom_temperature(),
+        target.get_weight(),
+        target.get_count(),
+        target.get_area_swept(),
+        target.get_num_records_aggregated()
+    )
+
+
+def download_and_persist_year(year: int, cursor: sqlite3.Cursor,
+    geohash_size: int, hauls: HAULS):
+    records = get_year(year, geohash_size, hauls)
+
+    persist_sql = get_sql('insert_record')
+    records_tuples = map(record_to_tuple, records)
+
+    cursor.executemany(persist_sql, records_tuples)
+
+
+def create_db_main(args):
+    if len(args) != USAGE_CREATE_DB_NUM_ARGS:
+        print(USAGE_BASE_STR + USAGE_CREATE_DB_STR)
+        return
+
+    filepath = args[0]
+    sql = get_sql('create_table')
+
+    # Thanks https://stackoverflow.com/questions/19522505
+    with contextlib.closing(sqlite3.connect(filepath)) as con:
+        with con as cur:
+            con.execute(sql)
+
+
+def create_download_main(args):
+    if len(args) != USAGE_DOWNLOAD_NUM_ARGS:
+        print(USAGE_BASE_STR + USAGE_DOWNLOAD_STR)
+        return
+
+    year_range_maybe = try_parse_range(args[0])
+    if year_range_maybe is None or len(year_range_maybe) != 2:
+        print(YEAR_RANGE_STR)
+        return
+
+    start_year = year_range_maybe[0]
+    end_year = year_range_maybe[1]
+
+    filepath = args[1]
+
+    try:
+        geohash_size = int(args[2])
+        assert geohash_size >= 1
+        assert geohash_size <= 12
+    except ValueError, AssertionError:
+        print(INVALID_GEOHASH_STR)
+        return
+    
+    years = range(start_year, end_year + 1)
+
+    # Thanks https://stackoverflow.com/questions/19522505
+    with contextlib.closing(sqlite3.connect(filepath)) as con:
+        for year in years:
+
+            with con as cur:
+                download_and_persist_year(year, cur, geohash_size, hauls)
+            
+            print('Completed %d.' % year)
+
+def main():
+    if len(sys.argv) < USAGE_BASE_NUM_ARGS:
+        print(USAGE_BASE_STR + USAGE_COMMANDS_STR)
+        return
+
+    command = sys.argv[1]
+
+    commands = {
+        USAGE_CREATE_DB_COMMAND: create_db_main,
+        USAGE_DOWNLOAD_COMMAND: download_main
+    }
+
+    if not command in commands:
+        print(USAGE_BASE_STR + USAGE_COMMANDS_STR)
+        return
+
+    commands[command](sys.argv[2:])
---
+++ app/model.py
@@ -0,0 +1,95 @@
+class SimplifiedRecord:
+
+    def __init__(self, year: int, survey: str, species: str, common_name: str,
+        geohash: str, surface_temperature: float, bottom_temperature: float,
+        weight: float, count: float, area_swept: float,
+        num_records_aggregated: int):
+        self._year = year
+        self._survey = survey
+        self._species = species
+        self._common_name = common_name
+        self._geohash = geohash
+        self._surface_temperature = surface_temperature
+        self._bottom_temperature = bottom_temperature
+        self._weight = weight
+        self._count = count
+        self._area_swept = area_swept
+        self._num_records_aggregated = num_records_aggregated
+
+    def get_year(self) -> int:
+        return self._year
+
+    def get_survey(self) -> str:
+        return self._survey
+
+    def get_species(self) -> str:
+        return self._species
+
+    def get_common_name(self) -> str:
+        return self._common_name
+    
+    def get_geohash(self) -> str:
+        return self._geohash
+    
+    def get_surface_temperature(self) -> float:
+        return self._surface_temperature
+    
+    def get_bottom_temperature(self) -> float:
+        return self._bottom_temperature
+    
+    def get_weight(self) -> float:
+        return self._weight
+    
+    def get_count(self) -> float:
+        return self._count
+    
+    def get_area_swept(self) -> float:
+        return self._area_swept
+
+    def get_num_records_aggregated(self) -> int:
+        return self._num_records_aggregated
+
+    def get_weight_by_area(self) -> float:
+        return self.get_weight() / self.get_area_swept()
+
+    def get_count_by_area(self) -> float:
+        return self.get_count() / self.get_area_swept()
+
+    def get_key(self) -> str:
+        pieces = [
+            self.get_year(),
+            self.get_survey(),
+            self.get_species(),
+            self.get_common_name(),
+            self.get_geohash()
+        ]
+        pieces_str = map(str, pieces)
+        return '\t'.join(pieces_str)
+
+    def combine(self, other: 'SimplifiedRecord') -> 'SimplifiedRecord':
+        assert self.get_key() == other.get_key()
+        
+        self_count = self.get_num_records_aggregated()
+        other_count = other.get_num_records_aggregated()
+        
+        surface_temp = self.get_surface_temperature() * self_count
+        surface_temp += other.get_surface_temperature() * other_count
+        surface_temp = surface_temp / (self_count + other_count)
+        
+        bottom_temp = self.get_bottom_temperature() * self_count
+        bottom_temp += other.get_bottom_temperature() * other_count
+        bottom_temp = bottom_temp / (self_count + other_count)
+        
+        return SimplifiedRecord(
+            self._year,
+            self._survey,
+            self._species,
+            self._common_name,
+            self._geohash,
+            surface_temp,
+            bottom_temp,
+            self.get_weight() + other.get_weight(),
+            self.get_count() + other.get_count(),
+            self.get_area_swept() + other.get_area_swept(),
+            self_count + other_count
+        )
