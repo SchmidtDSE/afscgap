34ddb5c262119a635d63698ee6ff001fab768b57
Sam Pottinger <sam.pottinger@berkeley.edu>
2023-04-28 09:23:52 -0700

    Update install browser script for Firefox.

--- install_browser.sh
+++ install_browser.sh
@@ -1,1 +1,4 @@
-sudo apt install -y chromium-browser
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F
+sudo apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu bionic main"
+sudo apt-get update
+sudo apt-get install firefox
