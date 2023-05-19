6704a1b614f6984e044ae5a2e2bc97ce5adaf67b
A Samuel Pottinger <sam@gleap.org>
2023-05-19 11:09:16 -0700

    Move install step for docs into build_docs.

--- build_docs.sh
+++ build_docs.sh
@@ -1,8 +1,11 @@
-echo "[1 / 2] Build API documentation..."
+echo "[1 / 3] Setup environment..."
+pip install -e .[dev]
+
+echo "[2 / 3] Build API documentation..."
 pdoc --docformat google ./afscgap -o website/devdocs/
 [[ ! -f website/devdocs/afscgap.html ]] && exit 1
 
-echo "[2 / 2] Build paper preview..."
+echo "[3 / 3] Build paper preview..."
 cd inst
 bash preview_paper.sh
 [[ ! -f ../website/static/paper.pdf ]] && exit 1
