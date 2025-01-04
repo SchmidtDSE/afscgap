f55632b602f9462b0379e631b440da5e4f0a806d
A Samuel Pottinger <sam@gleap.org>
2025-01-04 21:22:19 +0000

    Update test request source.
    Update tests for the request_source script as part of #111.

--- snapshot/test_request_source.py
+++ snapshot/test_request_source.py
@@ -16,17 +16,17 @@ import request_source
 class ApiUrlGenerationTests(unittest.TestCase):
 
     def test_haul(self):
-        url = request_source.get_api_request_url('haul', None, 12, 34)
+        url = request_source.get_api_request_url('haul', 2025, 12, 34)
         self.assertTrue('afsc_groundfish_survey_haul' in url)
         self.assertTrue('offset=12' in url)
         self.assertTrue('limit=34' in url)
+        self.assertTrue('q={"year":2025}' in url)
 
     def test_catch(self):
-        url = request_source.get_api_request_url('catch', 2025, 12, 34)
+        url = request_source.get_api_request_url('catch', None, 12, 34)
         self.assertTrue('afsc_groundfish_survey_catch' in url)
         self.assertTrue('offset=12' in url)
         self.assertTrue('limit=34' in url)
-        self.assertTrue('q={"year":2025}' in url)
 
     def test_species(self):
         url = request_source.get_api_request_url('species', None, 12, 34)
