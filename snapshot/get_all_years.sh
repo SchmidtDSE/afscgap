131c09e1f3636bee828810a499704c9691a66a7b
A Samuel Pottinger <sam@gleap.org>
2025-01-04 01:17:09 +0000

    Add additional bash scripts for #114.

---
+++ snapshot/get_all_years.sh
@@ -0,0 +1,90 @@
+echo "-- Getting species --"
+python request_source.py species $BUCKET_NAME species
+echo "-- Getting catch --"
+python request_source.py catch $BUCKET_NAME catch
+echo "-- Getting 1982 --"
+python request_source.py haul $BUCKET_NAME haul 1982
+echo "-- Getting 1983 --"
+python request_source.py haul $BUCKET_NAME haul 1983
+echo "-- Getting 1984 --"
+python request_source.py haul $BUCKET_NAME haul 1984
+echo "-- Getting 1985 --"
+python request_source.py haul $BUCKET_NAME haul 1985
+echo "-- Getting 1986 --"
+python request_source.py haul $BUCKET_NAME haul 1986
+echo "-- Getting 1987 --"
+python request_source.py haul $BUCKET_NAME haul 1987
+echo "-- Getting 1988 --"
+python request_source.py haul $BUCKET_NAME haul 1988
+echo "-- Getting 1989 --"
+python request_source.py haul $BUCKET_NAME haul 1989
+echo "-- Getting 1990 --"
+python request_source.py haul $BUCKET_NAME haul 1990
+echo "-- Getting 1991 --"
+python request_source.py haul $BUCKET_NAME haul 1991
+echo "-- Getting 1992 --"
+python request_source.py haul $BUCKET_NAME haul 1992
+echo "-- Getting 1993 --"
+python request_source.py haul $BUCKET_NAME haul 1993
+echo "-- Getting 1994 --"
+python request_source.py haul $BUCKET_NAME haul 1994
+echo "-- Getting 1995 --"
+python request_source.py haul $BUCKET_NAME haul 1995
+echo "-- Getting 1996 --"
+python request_source.py haul $BUCKET_NAME haul 1996
+echo "-- Getting 1997 --"
+python request_source.py haul $BUCKET_NAME haul 1997
+echo "-- Getting 1998 --"
+python request_source.py haul $BUCKET_NAME haul 1998
+echo "-- Getting 1999 --"
+python request_source.py haul $BUCKET_NAME haul 1999
+echo "-- Getting 2000 --"
+python request_source.py haul $BUCKET_NAME haul 2000
+echo "-- Getting 2001 --"
+python request_source.py haul $BUCKET_NAME haul 2001
+echo "-- Getting 2002 --"
+python request_source.py haul $BUCKET_NAME haul 2002
+echo "-- Getting 2003 --"
+python request_source.py haul $BUCKET_NAME haul 2003
+echo "-- Getting 2004 --"
+python request_source.py haul $BUCKET_NAME haul 2004
+echo "-- Getting 2005 --"
+python request_source.py haul $BUCKET_NAME haul 2005
+echo "-- Getting 2006 --"
+python request_source.py haul $BUCKET_NAME haul 2006
+echo "-- Getting 2007 --"
+python request_source.py haul $BUCKET_NAME haul 2007
+echo "-- Getting 2008 --"
+python request_source.py haul $BUCKET_NAME haul 2008
+echo "-- Getting 2009 --"
+python request_source.py haul $BUCKET_NAME haul 2009
+echo "-- Getting 2010 --"
+python request_source.py haul $BUCKET_NAME haul 2010
+echo "-- Getting 2011 --"
+python request_source.py haul $BUCKET_NAME haul 2011
+echo "-- Getting 2012 --"
+python request_source.py haul $BUCKET_NAME haul 2012
+echo "-- Getting 2013 --"
+python request_source.py haul $BUCKET_NAME haul 2013
+echo "-- Getting 2014 --"
+python request_source.py haul $BUCKET_NAME haul 2014
+echo "-- Getting 2015 --"
+python request_source.py haul $BUCKET_NAME haul 2015
+echo "-- Getting 2016 --"
+python request_source.py haul $BUCKET_NAME haul 2016
+echo "-- Getting 2017 --"
+python request_source.py haul $BUCKET_NAME haul 2017
+echo "-- Getting 2018 --"
+python request_source.py haul $BUCKET_NAME haul 2018
+echo "-- Getting 2019 --"
+python request_source.py haul $BUCKET_NAME haul 2019
+echo "-- Getting 2020 --"
+python request_source.py haul $BUCKET_NAME haul 2020
+echo "-- Getting 2021 --"
+python request_source.py haul $BUCKET_NAME haul 2021
+echo "-- Getting 2022 --"
+python request_source.py haul $BUCKET_NAME haul 2022
+echo "-- Getting 2023 --"
+python request_source.py haul $BUCKET_NAME haul 2023
+echo "-- Getting 2024 --"
+python request_source.py haul $BUCKET_NAME haul 2024
---
+++ snapshot/index_data.sh
@@ -0,0 +1,77 @@
+mkdir index_shards
+echo "area_swept_km2"
+python generate_indicies.py $BUCKET_NAME area_swept_km2 n
+echo "bottom_temperature_c"
+python generate_indicies.py $BUCKET_NAME bottom_temperature_c n
+echo "common_name"
+python generate_indicies.py $BUCKET_NAME common_name n
+echo "count"
+python generate_indicies.py $BUCKET_NAME count n
+echo "cpue_kgkm2"
+python generate_indicies.py $BUCKET_NAME cpue_kgkm2 n
+echo "cpue_nokm2"
+python generate_indicies.py $BUCKET_NAME cpue_nokm2 n
+echo "cruise"
+python generate_indicies.py $BUCKET_NAME cruise n
+echo "cruisejoin"
+python generate_indicies.py $BUCKET_NAME cruisejoin n
+echo "date_time"
+python generate_indicies.py $BUCKET_NAME date_time n
+echo "depth_m"
+python generate_indicies.py $BUCKET_NAME depth_m n
+echo "distance_fished_km"
+python generate_indicies.py $BUCKET_NAME distance_fished_km n
+echo "duration_hr"
+python generate_indicies.py $BUCKET_NAME duration_hr n
+echo "haul"
+python generate_indicies.py $BUCKET_NAME haul n
+echo "hauljoin"
+python generate_indicies.py $BUCKET_NAME hauljoin n
+echo "id_rank"
+python generate_indicies.py $BUCKET_NAME id_rank n
+echo "latitude_dd_end"
+python generate_indicies.py $BUCKET_NAME latitude_dd_end n
+echo "latitude_dd_start"
+python generate_indicies.py $BUCKET_NAME latitude_dd_start n
+echo "longitude_dd_end"
+python generate_indicies.py $BUCKET_NAME longitude_dd_end n
+echo "longitude_dd_start"
+python generate_indicies.py $BUCKET_NAME longitude_dd_start n
+echo "net_height_m"
+python generate_indicies.py $BUCKET_NAME net_height_m n
+echo "net_width_m"
+python generate_indicies.py $BUCKET_NAME net_width_m n
+echo "performance"
+python generate_indicies.py $BUCKET_NAME performance n
+echo "requirements"
+python generate_indicies.py $BUCKET_NAME requirements n
+echo "scientific_name"
+python generate_indicies.py $BUCKET_NAME scientific_name n
+echo "species_code"
+python generate_indicies.py $BUCKET_NAME species_code n
+echo "srvy"
+python generate_indicies.py $BUCKET_NAME srvy n
+echo "station"
+python generate_indicies.py $BUCKET_NAME station n
+echo "stratum"
+python generate_indicies.py $BUCKET_NAME stratum n
+echo "surface_temperature_c"
+python generate_indicies.py $BUCKET_NAME surface_temperature_c n
+echo "survey"
+python generate_indicies.py $BUCKET_NAME survey n
+echo "survey_definition_id"
+python generate_indicies.py $BUCKET_NAME survey_definition_id n
+echo "survey_name"
+python generate_indicies.py $BUCKET_NAME survey_name n
+echo "taxon_confidence"
+python generate_indicies.py $BUCKET_NAME taxon_confidence n
+#echo "variables"
+#python generate_indicies.py $BUCKET_NAME variables n
+echo "vessel_id"
+python generate_indicies.py $BUCKET_NAME vessel_id n
+echo "vessel_name"
+python generate_indicies.py $BUCKET_NAME vessel_name n
+echo "weight_kg"
+python generate_indicies.py $BUCKET_NAME weight_kg n
+echo "year"
+python generate_indicies.py $BUCKET_NAME year y
