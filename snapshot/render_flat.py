359d81ce9a244dd61eb0e817597d544de2da38ce
A Samuel Pottinger <sampottinger@users.noreply.github.com>
2025-01-05 12:44:06 -0800

    Fix render_flat check file exists
    Fix render_flat check file exists to use Bucket and Key keywords as
    part of #111.

--- snapshot/render_flat.py
+++ snapshot/render_flat.py
@@ -379,7 +379,7 @@ def process_haul(bucket: str, year: int, survey: str, haul: int,
             True if the file is found and false otherwise.
         """
         try:
-            s3_client.head_object(bucket=bucket, key=full_loc)
+            s3_client.head_object(Bucket=bucket, Key=full_loc)
             return True
         except botocore.exceptions.ClientError as e:
             error_code = e.response['Error']['Code']
